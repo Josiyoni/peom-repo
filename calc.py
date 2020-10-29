23a0b66a8903d038ac55c0658fdfa92944406331
Yosief27 <josiyoniug@gmail.com>
2020-10-29 10:51:12 +0100

    just modify message of add calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+def add(x,y):
+	return x+y
+def sub(x,y):
+	return x-y
+def mult(x,y):
+	return x*y
+def div(x,y):
+	if y==0:
+		raise ValueError("can't divide by zelo")
+	else:
+		return x/y
+
+def main():
+	print(add(4,5))
+	print(sub(5,3))
+if __name__=='__main__':
+	main()
